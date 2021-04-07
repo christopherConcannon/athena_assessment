5cf7304bf17bf861e562e96f6161dd3c0a74af03
xtophe xanon <xocon10@gmail.com>
2021-04-07 09:42:23 -0500

    prune features to sync with main branch

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import data from './data'
 
 import DataTable from './components/DataTable'
 
-
 function App() {
-	const [ data, setData ] = useState([])
-	const [ loading, setLoading ] = useState(true)
-	const [ error, setError ] = useState(false)
-
-	useEffect(() => {
-		const API_URL = 'http://localhost:3001/students'
-		const loadData = async () => {
-			try {
-				const res = await fetch(API_URL)
-				if (!res.ok) {
-					throw new Error('Could not fetch your data')
-				}
-				const json = await res.json()
-				setData(json)
-				setLoading(false)
-			} catch (err) {
-				setError(true)
-				setLoading(false)
-				console.log(err)
-			}
-		}
-		loadData()
-	}, [])
-
-
-	if (loading) {
-		return <h2>Loading...</h2>
-	}
 
 	return (
 		<div className='App'>
 			<div className='container'>
-
 				<DataTable data={data} />
-				{error && (
-					<div className='error'>Sorry, there was a problem loading your data</div>
-				)}
 			</div>
 		</div>
 	)
