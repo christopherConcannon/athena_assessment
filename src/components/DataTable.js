51959fb0be4d12b50ed415a73e1fdad589c71e11
xtophe xanon <xocon10@gmail.com>
2021-04-07 11:49:12 -0500

    refactor develop branch

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { average } from '../utils/helpers'
 
 const DataTable = ({ data }) => {
+	const [ sortDirection, setSortDirection ] = useState(null)
 
-  const rankedStudents = []
-  
-  data.forEach(student => {
-    student.avg = average(Object.values(student.scores))
-    rankedStudents.push(student)
-  })
+	data
+		.map((student) => {
+			student.avg = average(Object.values(student.scores))
+			return student
+		})
+		.sort((a, b) => b.avg - a.avg)
+		.forEach((student, idx) => (student.standing = idx + 1))
 
-  console.log(rankedStudents)
+	if (sortDirection !== null) {
+		data.sort((a, b) => {
+			if (a.standing < b.standing) {
+        return sortDirection === 'ascending' ? -1 : 1
+      }
+			if (a.standing > b.standing) {
+        return sortDirection === 'ascending' ? 1 : -1
+      }
+			return 0
+		})
+	}
 
-  rankedStudents.sort((a, b) => {
-    // descending sort
-    if (a.avg > b.avg) return -1
-    if (a.avg < b.avg) return 1
-    return 0
-  }).forEach((student, idx) => student.standing = idx + 1)
+	const handleSort = () => {
+    let direction = 'ascending'
+    if (sortDirection === 'ascending') direction = 'descending'
+    setSortDirection(direction)
+	}
 
 	return (
 		<div className='DataTable'>
@@ -30,7 +41,9 @@ const DataTable = ({ data }) => {
 						<th>Test 2</th>
 						<th>Test 3</th>
 						<th>Avg</th>
-						<th>Standing</th>
+						<th onClick={handleSort}>
+							Standing <i className='fas fa-sort' />
+						</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -41,8 +54,8 @@ const DataTable = ({ data }) => {
 							<td>{student.scores.test1}</td>
 							<td>{student.scores.test2}</td>
 							<td>{student.scores.test3}</td>
-              <td>{Math.round(student.avg)}</td>
-              <td>{student.standing}</td>
+							<td>{Math.round(student.avg)}</td>
+							<td>{student.standing}</td>
 						</tr>
 					))}
 				</tbody>
--- src/utils/helpers_backup.js
+++
@@ -1,3 +0,0 @@
-export const average = nums => {
-  return nums.reduce((acc, cur) => acc + cur, 0) / nums.length
-}
